d07514bc74e47902b9ad9aa3a8cabb49ec37f501
JonasCesar <jonascesar95@gmail.com>
2022-07-14 15:47:52 -0300

    Payment, one-to-one association

--- src/main/java/com/jonas/course/entities/Order.java
+++ src/main/java/com/jonas/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -38,6 +40,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	private Order() {
 		
@@ -71,6 +76,10 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+	
 	//-------------------------------
 
 	public void setId(Long id) {
@@ -89,6 +98,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
